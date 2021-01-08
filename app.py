aced49c714accd50b8add498b99fb85856382d1f
Osvaldo Ferraz <osvaldoferraz@Osvaldos-MacBook-Pro.local>
2021-01-08 00:48:26 -0600

    Flask running

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Flask import Flask, render_template
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import scraping
 
--- scraping.py
+++ scraping.py
@@ -2,13 +2,29 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
 
 # Patch to ChromDriver
-!which chromedriver
+# which chromedriver
 
-# Set the executable path and initialize the chrome brownse in splinter
-executable_path = {'executable_path': '/Users/osvaldoferraz/Desktop/Development/chromedriver'}
-browser = Browser('chrome', **executable_path)
+def scrap_all():
+    # Set the executable path and initialize the chrome brownse in splinter
+    executable_path = {'executable_path': '/Users/osvaldoferraz/Desktop/Development/chromedriver'}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    #Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 def mars_news(browser):
 
@@ -88,6 +104,9 @@ def mars_facts():
     #COnvert dataframe into HTML format, add bootstrap
     df.to_html()
 
+if __name__ == "__main__":
+    #if returning as script, print scraped data
+    print(scrape_all())
 
 
 
